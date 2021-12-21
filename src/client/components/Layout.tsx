59d5f04e7b77ca6afd997f76ea8898347ef33c6c
Srdjan S <stankovic.srdjo@gmail.com>
2021-12-21 14:20:28 +0100

    Fix layout props

--- src/client/components/Layout.tsx
+++ src/client/components/Layout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Fragment } from "react";
 
-export default function Layout(props) {
+type Props = {
+  children: JSX.Element
+}
+
+const Layout: React.FC<Props> = (props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Fragment>{props.children}</Fragment>
     </div>
   );
-}
+};
+
+export default Layout;
